68c090a33d5578024d68a0e4651f459b6fe5d736
Felix Kirchhoff <felix.kirchhoff@neuefische.de>
2022-11-23 10:48:37 +0100

    add push functionality

--- clone-session.js
+++ clone-session.js
@@ -8,48 +8,93 @@ import { exec } from "node:child_process";
     await fs.readFile("config.json")
   );
 
+  if (!curriculumPath || !coursePath) {
+    console.error(
+      "Error: Specify the paths to the web-curriculum-new-format and course repository in the config.json."
+    );
+    return;
+  }
+
   const autoPush = process.argv.includes("--push");
 
   const sessionPath = `${coursePath}/sessions/${session}`;
-  const copyTargets = [`${session}.md`, `challenges-${session}.md`, "assets"];
 
   console.log("Pulling latest changes for web-curriculum-new-format...");
 
-  exec(
-    `git --git-dir ${curriculumPath}/.git checkout main && git --git-dir ${curriculumPath}/.git pull`,
-    async (error, stdout) => {
-      if (error) {
-        console.log(error);
-        return;
-      }
-
-      console.log("done!");
-      console.log("Creating new session folder...");
-
-      try {
-        await fs.mkdir(sessionPath);
-        console.log("done!");
-      } catch (error) {
-        console.log(error.message);
-      }
-
-      console.log("copying files...");
-
-      for (const target of copyTargets) {
-        try {
-          await fs.cp(
-            `${curriculumPath}/sessions/${session}/${target}`,
-            `${sessionPath}/${target}`,
-            {
-              recursive: true,
-              force: true,
-            }
-          );
-          console.log(`Copied ${target}.`);
-        } catch (error) {
-          console.log(error.message);
+  try {
+    await new Promise((resolve, reject) =>
+      exec(
+        [`cd ${curriculumPath}`, `git checkout main`, `git pull`].join(" && "),
+        (error, _stdout) => {
+          if (error) {
+            reject(error);
+            return;
+          }
+          resolve();
         }
-      }
+      )
+    );
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+
+  console.log("Creating new session folder...");
+
+  try {
+    await fs.mkdir(sessionPath);
+    console.log(`Created session folder ${session}.`);
+  } catch (error) {
+    console.log(error.message);
+  }
+
+  console.log("copying files...");
+  const copyTargets = [`${session}.md`, `challenges-${session}.md`, "assets"];
+
+  for (const target of copyTargets) {
+    try {
+      await fs.cp(
+        `${curriculumPath}/sessions/${session}/${target}`,
+        `${sessionPath}/${target}`,
+        {
+          recursive: true,
+          force: true,
+        }
+      );
+      console.log(`${target} ✅`);
+    } catch (error) {
+      console.error(error.message);
     }
-  );
+  }
+
+  if (autoPush) {
+    console.log("Pushing new session to remote...");
+    try {
+      await new Promise((reject, resolve) =>
+        exec(
+          [
+            `cd ${coursePath}`,
+            `git checkout main`,
+            `git pull`,
+            `git switch -c session-${session} || git switch session-${session}`,
+            `git add --all`,
+            `git commit -m 'chore: add session ${session}'`,
+            `git push -u origin session-${session}`,
+            `git checkout main`,
+          ].join(" && "),
+          (error, stdout) => {
+            if (error) {
+              console.error(error);
+              reject(error);
+              return;
+            }
+            console.log("Push successful!");
+            resolve();
+          }
+        )
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  }
 })();
